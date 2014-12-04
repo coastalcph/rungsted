8e70dba9e8b6e3c53ab07dfda45b1958651c3166
Anders Johannsen <anders@johannsen.com>
2014-12-04 23:57:08 +0100

    move from distutils to setuptools
    Files are now Cythonized manually

---
+++ setup.py
@@ -0,0 +1,62 @@
+import os
+from os.path import splitext
+import subprocess
+from setuptools import setup, Extension
+import numpy as np
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+extra_compile_args = ['-Wno-deprecated', '-Wno-unused-function', '-Wno-#warnings', '-Wno-deprecated-writable-strings',
+                      '-std=c++11', '-stdlib=libc++', '-mmacosx-version-min=10.8']
+
+cython_modules = [['rungsted/feat_map.pyx', 'rungsted/MurmurHash3.cpp'],
+                  ['rungsted/weights.pyx'],
+                  ['rungsted/struct_perceptron.pyx'],
+                  ['rungsted/input.pyx'],
+                  ['rungsted/decoding.pyx'],
+                  ['rungsted/decoding_pd.pyx'],
+                  ['rungsted/corruption.pyx']
+                  ]
+
+ext_modules = []
+
+for cython_files in cython_modules:
+    source_files = []
+    module_name = None
+    for fname in cython_files:
+        if fname.endswith("pyx"):
+            cython_fname = fname
+            fname = fname.replace(".pyx", ".cpp")
+            fname_exists = os.path.exists(fname)
+            if not fname_exists or (fname_exists and os.path.getmtime(cython_fname) > os.path.getmtime(fname)):
+                print "calling cython"
+                subprocess.check_call(["cython", "--cplus", "--timestamps", cython_fname,
+                                       "--output-file", fname, '-v'])
+
+            if not module_name:
+                lead_path, ext = splitext(cython_fname)
+                module_name = lead_path.replace("/", ".")
+
+    source_files.append(fname)
+    ext_modules.append(Extension(module_name, sources=source_files,
+                       extra_compile_args=extra_compile_args, language='c++'))
+
+setup(
+    name="rungsted",
+    version="0.1",
+    author="Anders Johannsen",
+    author_email="ajohannsen@hum.ku.dk",
+    description=("Rungsted. A structured perceptron sequential tagger"),
+    keywords="hmm perceptron structured_model",
+    packages=['rungsted'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+    ],
+    ext_modules=ext_modules,
+    include_dirs = [np.get_include()]
+)
+
+
